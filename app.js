0622d877a5c099da46ae200873fa706a2dc55d93
Pauline Telan <pauline.telan@gmail.com>
2020-09-30 22:04:10 -0600

    adding about and contact pages

--- app.js
+++ app.js
@@ -16,18 +16,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {homeStartingContent: homeStartingContent});
+  res.render("home", {pageTitle: "Home", content: homeStartingContent});
 })
 
+app.get("/about", (req, res) => {
+  res.render("about", {pageTitle: "About", content: aboutContent});
+})
 
-
-
-
-
-
-
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact", {pageTitle: "Contact", content: contactContent});
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
